f10097dd97768e3698d1d4bc92c57bc3742abf4f
Jude <s3485744@student.rmit.edu.au>
2016-10-13 20:35:28 +1100

    Completed front end v1.0 New design implemented. Remaining bugs are for side scroll on excessive search tags. Fixing up appearance of Sliders for volume and timeline respectively. Any other final requests/discoveries.

--- src/site/header.php
+++ src/site/header.php
@@ -8,19 +8,22 @@
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="css/player.css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.structure.min.css">
+    <link rel="stylesheet" href="css/jquery-ui.theme.min.css">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,600" rel="stylesheet">
     <script src="js/jquery-3.1.0.min.js"></script>
-    <script src="js/soundcloud.player.api.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
 </head>
 <body>
 <header>
     <div class="nav">
         <a class="logo"><span class="logo_t">t</span>rebl.</a>
         <ul>
-            <li><a>Home</a></li>
-            <li><a>Generator</a></li>
-            <li><a>GitHub</a></li>
-            <li><a>About</a></li>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Generator</a></li>
+            <li><a href="#">GitHub</a></li>
+            <li><a href="#">About</a></li>
         </ul>
     </div>
 </header>
